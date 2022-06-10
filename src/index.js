b66454acd542626b695dbad53eba38d3905cdcac
Shashank Acharya <acharya.s@icloud.com>
2022-06-10 12:35:02 +0800

    updated to new template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+import { ApolloProvider } from '@apollo/client';
+import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import apolloClient from './services/apolloClientProvider';
 
+const theme = {
+	fontFamily: 'Inter',
+	primaryColor: 'orange'
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ApolloProvider client={apolloClient}>
+			<BrowserRouter>
+				<MantineProvider theme={theme}>
+					<App/>
+				</MantineProvider>
+			</BrowserRouter>
+		</ApolloProvider>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/services/apolloClientProvider.js
@@ -0,0 +1,24 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { AUTH_TOKEN } from '../utils/authToken'
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem(AUTH_TOKEN)
+	return {
+		headers: {
+			...headers,
+			authorization: `Bearer ${token}`
+		}
+	}
+})
+
+const httpLink = new HttpLink({
+	uri: "http://localhost:4000/graphql"
+})
+
+const apolloClient = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache()
+})
+
+export default apolloClient
