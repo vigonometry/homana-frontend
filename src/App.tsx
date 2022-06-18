bb44a6ce59f52f03b89678b049cee7f6a72143d9
Shashank Acharya <acharya.s@icloud.com>
2022-06-19 00:50:01 +0800

    add routes for claims and policies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import AppContainer from "./components/navigation/AppContainer";
+import ClaimsPage from "./pages/ClaimsPage";
 import DashboardPage from "./pages/DashboardPage";
+import PoliciesPage from "./pages/PoliciesPage";
 
 export default function App() {
 	return (
 		<AppContainer>
 			<Routes>
 				<Route path="/" element={<DashboardPage/>}/>
+				<Route path="/claims" element={<ClaimsPage/>}/>
+				<Route path="/policies" element={<PoliciesPage/>}/>
 			</Routes>
 		</AppContainer>
 	)
