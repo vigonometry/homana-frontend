098f96fcafe12d2b4fe6aebdcdee2108436aa8ce
Shashank Acharya <acharya.s@icloud.com>
2022-06-20 00:49:44 +0800

    added check for current user in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import AppContainer from "./components/navigation/AppContainer";
+import AuthPage from "./pages/AuthPage";
 import ClaimsPage from "./pages/ClaimsPage";
 import DashboardPage from "./pages/DashboardPage";
 import PoliciesPage from "./pages/PoliciesPage";
+import { CURRENT_USER } from "./queries/auth";
+import { User } from "./types/user";
 
 export default function App() {
+
+	const [user, setUser] = useState<User | null | undefined>(undefined)
+
+	const { loading, data } = useQuery(CURRENT_USER)
+
+	useEffect(() => {
+		if (data) setUser(data.currentUser)
+	}, [loading, data])
+
+	if (user === undefined) return <></>
+	if (user === null) return <AuthPage/>
+
 	return (
-		<AppContainer>
+		<AppContainer user={user}>
 			<Routes>
 				<Route path="/" element={<DashboardPage/>}/>
 				<Route path="/claims" element={<ClaimsPage/>}/>
