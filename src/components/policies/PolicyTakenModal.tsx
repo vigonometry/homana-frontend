55bf1d9432d7f5f5d0d9215c28c9afd276309426
Shashank Acharya <acharya.s@icloud.com>
2022-06-28 11:24:30 +0530

    added next step and cancel mutations for PT

--- src/components/policies/PolicyTakenModal.tsx
+++ src/components/policies/PolicyTakenModal.tsx
@@ -1,22 +1,48 @@
-import { Avatar, Badge, Button, Divider, Group, Modal, Stack, Text, Title } from "@mantine/core"
+import { useMutation } from "@apollo/client"
+import { Avatar, Badge, Button, Group, Modal, Stack, Text, Title } from "@mantine/core"
 import { useContext } from "react"
+import { POLICY_TAKEN_CANCEL, POLICY_TAKEN_NEXT_STEP } from "../../queries/policyTaken"
 import { UserContext } from "../../services/userContextProvider"
+import { Callbacks } from "../../types/callbacks"
 import { PolicyTaken } from "../../types/policy"
 import ptBadgeColor from "../../utils/ptBadgeColor"
 
 interface PolicyTakenModalProps {
 	policyTaken: PolicyTaken | null
 	close: () => void
+	callbacks: Callbacks<PolicyTaken>
 }
 
 function PolicyTakenModal(props: PolicyTakenModalProps) {
 	const { user } = useContext(UserContext)
+	console.log(props.policyTaken)
+	const [policyNext] = useMutation(POLICY_TAKEN_NEXT_STEP, {
+		variables: { _id: props.policyTaken?._id, status: props.policyTaken?.status},
+		onCompleted: ({ policyTakenNext }) => {
+			if (policyTakenNext.response && props.policyTaken) {
+				props.callbacks.update({...props.policyTaken, status: policyTakenNext.response})
+				props.close()
+			} else {
+				console.log(policyTakenNext.error)
+			}
+		}
+	})
+	const [policyCancel] = useMutation(POLICY_TAKEN_CANCEL, {
+		variables: { _id: props.policyTaken?._id, status: props.policyTaken?.status},
+		onCompleted: ({ policyTakenCancel }) => {
+			if (policyTakenCancel.response && props.policyTaken) {
+				props.callbacks.update({...props.policyTaken, status: policyTakenCancel.response})
+				props.close()
+			} else {
+				console.log(policyTakenCancel.error)
+			}
+		}
+	})
 	return (
 		<Modal opened={!!props.policyTaken} onClose={props.close} title={props.policyTaken?.policy?.title} styles={{title: { fontWeight: 'bold', fontSize: 22, }, header: { marginBottom: 4}}}>
 			<Badge size="md" color={ptBadgeColor(props.policyTaken?.status || '')}>{props.policyTaken?.status}</Badge>
-			<Stack spacing='sm' mt='lg'>
-				<Divider/>
-				<Stack spacing='xs' p='sm'>
+			<Stack spacing={36} mt={36}>
+				<Stack spacing='xs'>
 					<Title order={4}>Agent Details</Title>
 					<Group>
 						<Avatar size='md' color='grape'>{props.policyTaken?.agent?.name?.charAt(0)}</Avatar>
@@ -26,8 +52,7 @@ function PolicyTakenModal(props: PolicyTakenModalProps) {
 						</Stack>
 					</Group>
 				</Stack>
-				<Divider/>
-				<Stack spacing='xs' p='sm'>
+				<Stack spacing='xs'>
 					<Title order={4}>Quotation Details</Title>
 					<Group>
 						<Stack spacing={0}>
@@ -47,15 +72,14 @@ function PolicyTakenModal(props: PolicyTakenModalProps) {
 					</Group>
 				</Stack>
 				{
-					user?.__typename === 'Client' && props.policyTaken?.status === 'QUOTED' && (
+					(((user?.__typename === 'Client' && props.policyTaken?.status === 'QUOTED')) || ((user?.__typename === 'Broker' && props.policyTaken?.status === 'APPLIED'))) && (
 						<>
-							<Divider/>
 							<Stack spacing='xs' py='sm'>
-								<Group>
-									<Button>Sign and Apply</Button>
-									<Text size='xs' color='dimmed'>By clicking Sign and Apply, ...</Text>
+								<Group position="apart">
+									<Button onClick={() => policyNext()}>Sign and {user.__typename === 'Broker'? 'Approve' : 'Apply'}</Button>
+									<Button color='red' onClick={() => policyCancel()}>Reject</Button>
 								</Group>
-								
+								<Text size='xs' color='dimmed'>By clicking Sign and {user.__typename === 'Broker'? 'Approve' : 'Apply'}, ...</Text>
 							</Stack>
 						</>
 					)
--- src/queries/policyTaken.ts
+++ src/queries/policyTaken.ts
@@ -41,6 +41,26 @@ export const GET_CURRENT_POLICIES_TAKEN = gql`
 					premium
 				}
 			}
+			... on Broker {
+				policiesTaken {
+					_id
+					policy {
+						title
+						type
+					}
+					client {
+						name
+					}
+					agent {
+						name
+						email
+					}
+					date
+					status
+					insuredAmount
+					premium
+				}
+			}
 		}
 	}
 `
@@ -48,6 +68,24 @@ export const GET_CURRENT_POLICIES_TAKEN = gql`
 export const CREATE_POLICY_TAKEN = gql`
 	mutation CreatePolicyTaken($policyId: ID!, $clientEmail: ID!, $agentId: ID!, $insuredAmount: Float!, $premium: Float!) {
 		createPolicyTaken(policyId: $policyId, clientEmail: $clientEmail, agentId: $agentId, insuredAmount: $insuredAmount, premium: $premium) {
+			response
+			error
+		}
+	}
+`
+
+export const POLICY_TAKEN_NEXT_STEP = gql`
+	mutation PolicyTakenNext($_id: ID!, $status: Status!) {
+		policyTakenNext(_id: $_id, status: $status) {
+			response
+			error
+		}
+	}
+`
+
+export const POLICY_TAKEN_CANCEL = gql`
+	mutation PolicyTakenCancel($_id: ID!, $status: Status!) {
+		policyTakenCancel(_id: $_id, status: $status) {
 			response
 			error
 		}
