285ec6fd1433aa514abbbb774a17b991c06cdd08
Shashank Acharya <acharya.s@icloud.com>
2022-07-09 21:03:34 +0800

    added dashboards for customer

--- src/components/policies/PoliciesTakenGrid.tsx
+++ src/components/policies/PoliciesTakenGrid.tsx
@@ -10,6 +10,7 @@ import PolicyTakenModal from "./PolicyTakenModal";
 
 interface PoliciesTakenGridProps {
 	policiesTaken: PolicyTaken[]
+	withControls?: boolean
 }
 
 const CLIENT_MODES = ['Current', 'Inbox', 'Others']
@@ -30,10 +31,12 @@ function PoliciesTakenGrid(props: PoliciesTakenGridProps) {
 	return (
 		<Stack spacing='xl'>
 			<PolicyTakenModal callbacks={callbacks} policyTaken={current} close={close}/>
-			<Group>
-				<TextInput placeholder="Search" size="md"/>
-				<SegmentedControl value={mode} onChange={setMode} data={user.__typename === 'Broker' ? BROKER_MODES : CLIENT_MODES}/>
-			</Group>
+			{ props.withControls &&
+				<Group>
+					<TextInput placeholder="Search" size="md"/>
+					<SegmentedControl value={mode} onChange={setMode} data={user.__typename === 'Broker' ? BROKER_MODES : CLIENT_MODES}/>
+				</Group>
+			}
 			{ user.__typename === 'Client' && <SimpleGrid breakpoints={[{ minWidth: 'sm', cols: 1}, { minWidth: 'md', cols: 2}, { minWidth: 'lg', cols: 3}]}>
 				{props.policiesTaken.filter(p => mode === 'Current' ? p.status === 'APPROVED' : mode === 'Others' ? p.status !== 'QUOTED' && p.status !== 'APPROVED' : p.status === 'QUOTED').map(p => (
 					p.policy && <PolicyTakenCard policyTaken={p} onClick={handleClick(p)}/>
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,9 @@
-import { Group, Title } from "@mantine/core"
+import { Group, Stack, Title } from "@mantine/core"
+import { useContext } from "react"
+import ClaimsTable from "../components/claims/ClaimsTable"
 import PageContainer from "../components/containers/PageContainer"
+import PoliciesTakenGrid from "../components/policies/PoliciesTakenGrid"
+import { UserContext } from "../services/userContextProvider"
 
 export default function DashboardPage() {
 	return (
@@ -7,6 +11,38 @@ export default function DashboardPage() {
 			<Group position="apart">
 				<Title order={2}>Dashboard</Title>
 			</Group>
+			<CustomerDashboard/>
 		</PageContainer>
+	)
+}
+
+function CustomerDashboard() {
+	// RECENT CLAIMS + POLICIES
+	const { user } = useContext(UserContext)
+	return (
+		<>
+		<Stack mt='xl'>
+			<Title order={4}>Recent Claims</Title>
+			<ClaimsTable limit={5}/>
+		</Stack>
+		<Stack mt='xl'>
+			<Title order={4}>Your Policies</Title>
+			<PoliciesTakenGrid policiesTaken={user?.policiesTaken || []}/>
+		</Stack>
+		</>
+	)
+}
+
+function AgentDashboard() {
+	// CLAIMS + APPLICATIONS + NUMBER
+	return (
+		<></>
+	)
+}
+
+function BrokerDashboard() {
+	// CLAIMS + APPLICATIONS + NUMBER
+	return (
+		<></>
 	)
 }
